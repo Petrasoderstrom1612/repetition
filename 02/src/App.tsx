ec31f0960a7c5aa5128d6048380ee47bf6bfd1a1
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-07-20 12:09:12 +0200

    added a new todo

--- 02/src/App.tsx
+++ 02/src/App.tsx
@@ -6,11 +6,18 @@ interface Post {
     likes: number;
 }
 const App = () => {
- const [posts, setPosts] = useState<Post[]>([  //tom array är typ never
- {id: 1, title: "react rocks", likes: 33}, 
- {id: 2,  title: "jsx moar rocks", likes: 27},
- {id: 3,  title: "statefull?", likes: 433},
- ])
+  const [newTitle, setNewTitle] = useState("")
+  const [posts, setPosts] = useState<Post[]>([  //tom array är typ never
+  {id: 1, title: "react rocks", likes: 33}, 
+  {id: 2,  title: "jsx moar rocks", likes: 27},
+  {id: 3,  title: "statefull?", likes: 433},
+  ])
+
+  const addNewTodo = (e) => {
+    e.preventDefault()
+    setPosts([...posts, { id: Math.max(...posts.map(p => p.id)) + 1, title: newTitle, likes: 0}])
+    setNewTitle("")
+  }
 
   return (
     <>
@@ -25,6 +32,20 @@ const App = () => {
             </li>
         )}
         </ul>) : (<p>no posts...</p>)}
+
+      <form>
+        <input
+          placeholder="new todo item"
+          aria-label="new todo item"
+          type="text"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          name="new-todo-item"
+          required
+          // minlength="4"
+        />
+        <button type="submit" onClick={(e) =>addNewTodo(e)}>Add new todo</button>
+      </form>
     </>
   )
 }
