ee62990db6bd1f26ff21b0785fda561e32396507
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-07-27 14:52:05 +0200

    one way value added

--- 03/src/App.tsx
+++ 03/src/App.tsx
@@ -13,25 +13,40 @@ function App() {
     {id: 2, title: "react is best", likes: 200},
     {id: 3, title: "react is best", likes: 3000}
   ])
+  const [postTitle, setPostTitle] = useState("")
 
   const handleLike = (postId: number) =>{
-    setPosts(posts.map(p => p.id === postId ? {...p, likes: p.id + 1 } : p))
+    setPosts(posts.map(p => p.id === postId ? {...p, likes: p.id + 1 } : p)) //returns ternary
   }
 
   const removeLike = (postId: number) => {
-    setPosts(posts.filter(p => p.id !== postId))
+    setPosts(posts.filter(p => p.id !== postId)) //returns whatever is truthy
   }
 
   return (
-    <ul>
-    {posts.map(post => 
     <>
-    <li key={post.id}>{post.title} {post.likes}</li>
-    <button className="btn btn-success" onClick={() => handleLike(post.id)}>👍🏻</button>
-    <button className="btn btn-danger" onClick={() => removeLike(post.id)}>❌</button>
-    </>
-    )}
-    </ul>
+  {posts.length > 0 ?
+    (  <ul>
+        {posts.map(post => 
+        <>
+        <li key={post.id}>{post.title} {post.likes}</li>
+        <button className="btn btn-success" onClick={() => handleLike(post.id)}>👍🏻</button>
+        <button className="btn btn-danger" onClick={() => removeLike(post.id)}>❌</button>
+        </>
+        )}
+        </ul>
+    ) : <p>No posts...</p>}
+
+    <form>
+      <input
+        aria-label="create a new post"
+        minLength={2}
+        value={postTitle}
+        onChange={(e) => setPostTitle(e.target.value)}
+      />
+      <button type="submit" className="btn btn-primary">Create a new post</button>
+    </form>
+  </>
   )
 }
 
