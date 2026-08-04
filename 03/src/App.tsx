cc57da68c9dfa0509bd8ec299896483cc4409da9
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-08-04 14:26:06 +0200

    Added TodosList

--- 03/src/App.tsx
+++ 03/src/App.tsx
@@ -7,6 +7,7 @@ import Container from 'react-bootstrap/Container' //it is recommended to use thi
 import ListGroup from 'react-bootstrap/ListGroup'
 import type {Post} from "./types/Todo.types" // do not forget type!!! (you can also have { type X, Y Z}) if you want to group them
 import TodosListItem from "./components/TodosListItem"
+import TodosList from './components/TodosList'
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([
@@ -17,43 +18,36 @@ function App() {
 
   
   const doneCount = posts.filter(p => p.done).length
-  const incompletedPosts = posts.filter(post => !post.done)
-  const completedPosts = posts.filter(post => post.done)
-
+  
   const handleLike = (postId: number) =>{
     setPosts(posts.map(p => p.id === postId ? {...p, likes: p.likes + 1 } : p)) //returns ternary
   }
-
+  
   const removePost = (postId: number) => {
     console.log(postId)
     setPosts(posts.filter(p => p.id !== postId)) //returns whatever is truthy, keep all whose id is not matching the incoming id, the result will be a new array of filtered p's
   }
-
+  
   const changeDone = (post: Post) => {
     console.log(post.id)
     post.done = !post.done 
-      setPosts([...posts])
+    setPosts([...posts])
   }
-
+  
   const handleStateSetters = (title: string) => {
     setPosts([...posts, {id: Math.max(0,...posts.map(p => p.id)) + 1, title: title, likes: 0}]) //!do not forget 0 if no posts!!!! so it does not add infinity
   }
 
+  const incompletedPosts = posts.filter(post => !post.done)
+  const completedPosts = posts.filter(post => post.done)
+
   return (
     <Container>
-    <Clicker/>
-    <h1>Todos</h1>
-    <Counter completed={posts.filter(p => p.done).length} total={posts.length}/>
-    <AddNewTodoForm  handleStateSetters={handleStateSetters}/>
   <>
   {posts.length > 0 ?
     (  <>
         <h2 className="h5 mb-2">"Done stuff"</h2>
-        <ListGroup className="todolist mb-3">
-          {incompletedPosts.map(post => 
-        <TodosListItem handleLike={handleLike} removePost={removePost} changeDone={changeDone} post={post}/>
-        )}
-        </ListGroup>
+        <TodosList handleLike={handleLike} removePost={removePost} changeDone={changeDone} posts={incompletedPosts}/>
         <hr/>
         <h2 className="h5 mb-2">"To do stuff"</h2>
         <ListGroup className="todolist mb-3">
@@ -65,6 +59,10 @@ function App() {
         </>
     ) : <p>No posts...</p>}
   </>
+    <Clicker/>
+    <h1>Todos</h1>
+    <Counter completed={posts.filter(p => p.done).length} total={posts.length}/>
+    <AddNewTodoForm  handleStateSetters={handleStateSetters}/>
   </Container>
   )
 }
