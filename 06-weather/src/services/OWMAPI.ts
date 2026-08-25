bd136b61f86928d75232b205c7d6cf0b092819dc
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-08-25 08:50:15 +0200

    fake slow API to simulate slow environment

---
+++ 06-weather/src/services/OWMAPI.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 2000;
+
+/* Get current weather conditions for a city with a slow API 
+*  @param city City name
+*  @return Current weather conditions
+*/
+
+export const getCurrentWEather = async (city: string) => {
+    if (FAKE_SLOW_API) {
+        await new Promise((r) => setTimeout(r, FAKE_SLOW_API_DELAY))
+    }
+}
