9fa3d365aa19c2fa1ddaec6c6ab791d9af1866c9
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-08-25 11:09:28 +0200

    scss fixed

--- 06-weather/src/services/OWMAPI.ts
+++ 06-weather/src/services/OWMAPI.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+// import axios from "axios";
 
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
+// const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const FAKE_SLOW_API = true;
 const FAKE_SLOW_API_DELAY = 2000;
 
@@ -13,4 +13,5 @@ export const getCurrentWEather = async (city: string) => {
     if (FAKE_SLOW_API) {
         await new Promise((r) => setTimeout(r, FAKE_SLOW_API_DELAY))
     }
+    console.log(city)
 }
