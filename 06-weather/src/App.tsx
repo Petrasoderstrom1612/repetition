1cb574f9673f619562c7041efe4fee6b24177016
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-08-28 16:21:48 +0200

    had to import import "./services/OWMAPI" so it works

--- 06-weather/src/App.tsx
+++ 06-weather/src/App.tsx
@@ -1,6 +1,7 @@
 import './assets/scss/App.scss'
 import CurrentWeather from './components/CurrentWeather'
 import SearchCity from './components/SearchCity'
+import "./services/OWMAPI"
 
 function App() {
 
--- 06-weather/src/services/OWMAPI.ts
+++ 06-weather/src/services/OWMAPI.ts
@@ -1,17 +1,35 @@
-// import axios from "axios";
+import axios from "axios";
 
 // const BASE_URL = "https://api.openweathermap.org/data/2.5";
+//https://api.openweathermap.org/data/2.5/weather?q={london}&appid={93b8dc43f170a358bd8821511c0edff5}
 const FAKE_SLOW_API = true;
 const FAKE_SLOW_API_DELAY = 2000;
+
+const API_KEY = import.meta.env.VITE_OWN_APIKEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
 /* Get current weather conditions for a city with a slow API 
 *  @param city City name
 *  @return Current weather conditions
 */
 
-export const getCurrentWEather = async (city: string) => {
+export const getCurrentWeather = async (city: string) => {
+    const res = await axios.get(BASE_URL + "/weather",
+        { params: {
+            q: city,
+            units: "metric",
+            appid: API_KEY,
+        },}
+    )
+        // ?q=${city}&units=metric&appid=${API_KEY}`)
+
     if (FAKE_SLOW_API) {
         await new Promise((r) => setTimeout(r, FAKE_SLOW_API_DELAY))
     }
     console.log(city)
+
+    return res.data;
 }
+
+// const weather = await getCurrentWeather("Stockholm");
+// console.log(weather);
